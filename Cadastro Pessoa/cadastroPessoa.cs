81b764e657c49bd9e5f0ceaae948f913b6bcd288
mariana <marigigialcantara40@gmail.com>
2024-01-06 17:39:34 -0300

    update file

--- Cadastro Pessoa/cadastroPessoa.cs
+++ Cadastro Pessoa/cadastroPessoa.cs
@@ -4,11 +4,47 @@ class Pessoa
 {
   private string Nome { get; }
   private int Idade { get; }
+  private string Nacionalidade { get;using System;
 
-  public Pessoa (string nome, int idade)
+class Pessoa
+{
+    public string Nome { get; }
+    public int Idade { get; }
+    public string Nacionalidade { get; }
+
+    public Pessoa(string nome, int idade, string nacionalidade)
+    {
+        Nome = nome;
+        Idade = idade;
+        Nacionalidade = nacionalidade;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+      Console.WriteLine("Digite seu nome: ");
+      string nome = Console.ReadLine();
+
+      Console.WriteLine("Digite sua idade: ");
+      int idade = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Digite sua nacionalidade: ");
+      string nacionalidade = Console.ReadLine();
+
+      Pessoa pessoa = new Pessoa(nome, idade, nacionalidade);
+
+      Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, Nacionalidade: {pessoa.Nacionalidade}");
+    }
+}
+}
+
+  public Pessoa (string nome, int idade, string nacionalidade)
   {
     Nome = nome;
     Idade = idade;
+    Nacionalidade = nacionalidade;
   }
 }
 
@@ -19,8 +55,8 @@ class Program
     string nome = Console.ReadLine();
     int idade = Convert.ToInt32(Console.ReadLine());
 
-    Pessoa pessoa = new Pessoa(nome, idade);
+    Pessoa pessoa = new Pessoa(nome, idade, nacionalidade);
 
-    Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}");
+    Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, Nacionalidade: {pessoa.Nacionalidade}");
   }
 }
