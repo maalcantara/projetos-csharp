2977e17cd1def5a047e1612973cb386126b9a056
mariana <marigigialcantara40@gmail.com>
2024-01-15 14:33:52 -0300

    add class

---
+++ Testes Unitários/Calculadora/services/CalculadoraImp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Threadling.Tasks;
+
+namespace calculadora.services
+{
+  public class CalculadoraImp
+  {
+    public int Somar(int num1, int num2)
+    {
+      return num1 + num2;
+    }
+  }
+}
