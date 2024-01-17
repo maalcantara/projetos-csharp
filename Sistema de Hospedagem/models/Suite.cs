ef1b6e5bb9909d95a8e7bf84853def32b2ab234b
mariana <marigigialcantara40@gmail.com>
2024-01-17 14:45:55 -0300

    add class

---
+++ Sistema de Hospedagem/models/Suite.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema de Hospedagem.models
+{
+    public class Suite
+    {
+        public Suite() { }
+
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+    }
+}
