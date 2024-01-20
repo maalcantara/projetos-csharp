c0a3aecf7dd11774c2d0656e1b6c825241c519ab
mariana <marigigialcantara40@gmail.com>
2024-01-19 21:00:17 -0300

    update class

--- Serialização/Models/Venda.cs
+++ Serialização/Models/Venda.cs
@@ -7,33 +7,37 @@ using Newtonsoft.Json;
 namespace Csharp.projetos-csharp.Serialização.models
 {
     // DESSERIALIZAÇÃO
-    public class Venda 
-    {
-        public int Id { get; set; }
-
-        //atributo para modificar a prop do json
-        [JsonProperty("Nome_Produto")]
-        public string Produto { get; set; }
-        
-        public decimal Preco { get; set; }
-        public DateTime DataVenda { get; set; }
-    }
-
-    // SERIALIZAÇÃO
     public class Venda
-    {
-        // construtor
-        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
-        {
-            Id = id;
-            Produto = produto;
-            Preco = preco;
-            DataVenda = dataVenda;
-        }
+{
+    public int Id { get; set; }
 
-        public int Id { get; set; }
-        public string Produto { get; set; }
-        public decimal Preco { get; set; }
-        public DateTime DataVenda { get; set; }
+    //atributo para modificar a prop do json
+    [JsonProperty("Nome_Produto")]
+    public string Produto { get; set; }
+
+    public decimal Preco { get; set; }
+    public DateTime DataVenda { get; set; }
+    public decimal? Desconto { get; set; }
+}
+
+// SERIALIZAÇÃO
+public class Venda
+{
+    // construtor
+    // a prop Desconto pode ser um null type
+    public Venda(int id, string produto, decimal preco, DateTime dataVenda, decimal? desconto)
+    {
+        Id = id;
+        Produto = produto;
+        Preco = preco;
+        DataVenda = dataVenda;
+        Desconto = desconto;
     }
+
+    public int Id { get; set; }
+    public string Produto { get; set; }
+    public decimal Preco { get; set; }
+    public DateTime DataVenda { get; set; }
+    public decimal? Desconto { get; set; }
+}
 }
