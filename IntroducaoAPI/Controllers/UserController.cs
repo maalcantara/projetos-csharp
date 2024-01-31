7f2d8d597e82c95acdbbdb9a3a2a8925e9f6a99c
mariana <marigigialcantara40@gmail.com>
2024-01-31 19:46:26 -0300

    add controller

---
+++ IntroducaoAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace moduloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        // retornar data e hora atual
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+            
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var msg = $"Olá {nome}, seja bem vindo!";
+            return Ok(new {msg});
+        }
+    }
+}
