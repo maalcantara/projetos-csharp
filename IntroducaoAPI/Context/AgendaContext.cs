d87b3e7d5783348a8bb1aeb59f071d062e04bc76
mariana <marigigialcantara40@gmail.com>
2024-02-06 19:14:43 -0300

    add Context

---
+++ IntroducaoAPI/Context/AgendaContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using IntroducaoAPI.Entities;
+
+namespace IntroducaoAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbConxetOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        // propriedade que refere a Entidade -> classe no programa e ao mesmo tempo uma tabela no db
+        public DbSet<Contato> Contatos { get; set; }
+
+    }
+}
