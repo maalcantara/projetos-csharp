2c6bb3689677c8d24e2ed6e2d41bfbe1a53cb597
mariana <marigigialcantara40@gmail.com>
2024-03-03 16:18:28 -0300

    add context Agenda

---
+++ MVC/Context/AgendaContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+
+namespace MVC.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
